a4efd2dbef71ebdab2101f0faff5ccb5d4ad4b83
Kasey Dixon <kasey.dixon@kochind.com>
2026-01-22 14:30:12 -0600

    Amplify CLI commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifygen1test": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
